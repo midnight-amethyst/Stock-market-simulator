a8043523943e105442c203832887081b24d45e15
Marek <20026557-MDAmethyst@users.noreply.replit.com>
2024-04-18 11:01:36 +0000

    Added to stockMarket class

--- main.py
+++ main.py
@@ -6,10 +6,21 @@ class stockMarket:
         Format for self.stocks dict
 
         {
-            name: 
-        
+            str :{
+            sharePrice: float
+            marketCap: float
+            maxShares: int
+            outstandingShares: int
+            askingPrice: float
+            sharesGoDownRate: float
+            previousEventRating: int
+            }
         }
         
         '''
         self.stocks = {}
+        self.averageGrowth = 1.102 # Gains 10.2% every year according to bing
+        self.inlfation = 0
+        self.time = 0
+
 
