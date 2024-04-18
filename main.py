acad2ac40fbd195a96dfbd995f95de2290ae02ba
Marek <20026557-MDAmethyst@users.noreply.replit.com>
2024-04-18 10:35:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+
+class stockMarket:
+    def __init__(self):
+        '''
+        Format for self.stocks dict
+
+        {
+            name: 
+        
+        }
+        
+        '''
+        self.stocks = {}
+
